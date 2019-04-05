33e00174a99e7a7572d41fb6ffc0387273b354b5
tiffylou <tiffany6872@gmail.com>
2019-04-04 23:28:15 -0400

    small changes to test react app working

--- src/App.js
+++ src/App.js
@@ -7,18 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Tiffany France - D3 React Example
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            
+          </div>
         </header>
       </div>
     );
