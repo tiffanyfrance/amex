0d96957b9c12c22235a8f5ba92ae6e6caa42e5eb
tiffylou <tiffany6872@gmail.com>
2019-04-05 08:36:30 -0400

    working histogram

--- src/App.js
+++ src/App.js
@@ -1,12 +1,128 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Histogram from './histogram.js';
 
 class App extends Component {
 
-  state = {
-    data: [12, 5, 6, 6, 9, 10],
+  products = {
+    data: [
+      {dtg: '01-01-2011', value: 3},
+      {dtg: '01-01-2011', value: 3},
+      {dtg: '01-01-2011', value: 3},
+      {dtg: '01-01-2011', value: 3.1},
+      {dtg: '01-01-2011', value: 3.1},
+      {dtg: '01-01-2011', value: 3.2},
+      {dtg: '01-01-2011', value: 3.2},
+      {dtg: '01-01-2011', value: 3.2},
+      {dtg: '01-01-2011', value: 3.3},
+      {dtg: '01-01-2011', value: 3.3},
+      {dtg: '01-01-2011', value: 3.4},
+      {dtg: '01-01-2011', value: 3.4},
+      {dtg: '01-01-2011', value: 3.8},
+      {dtg: '01-01-2011', value: 3.8},
+      {dtg: '01-01-2011', value: 4.4},
+      {dtg: '01-02-2011', value: 3},
+      {dtg: '01-02-2011', value: 3},
+      {dtg: '01-02-2011', value: 3.2},
+      {dtg: '01-02-2011', value: 3.4},
+      {dtg: '01-02-2011', value: 3.4},
+      {dtg: '01-02-2011', value: 3.4},
+      {dtg: '01-02-2011', value: 3.5},
+      {dtg: '01-02-2011', value: 3.6},
+      {dtg: '01-03-2011', value: 3},
+      {dtg: '01-03-2011', value: 3},
+      {dtg: '01-03-2011', value: 3},
+      {dtg: '01-03-2011', value: 3.1},
+      {dtg: '01-03-2011', value: 3.1},
+      {dtg: '01-03-2011', value: 3.1},
+      {dtg: '01-03-2011', value: 3.1},
+      {dtg: '01-03-2011', value: 3.1},
+      {dtg: '01-03-2011', value: 3.3},
+      {dtg: '01-03-2011', value: 3.3},
+      {dtg: '01-03-2011', value: 3.3},
+      {dtg: '01-03-2011', value: 3.4},
+      {dtg: '01-03-2011', value: 3.4},
+      {dtg: '01-03-2011', value: 3.5},
+      {dtg: '01-03-2011', value: 3.5},
+      {dtg: '01-03-2011', value: 3.6},
+      {dtg: '01-03-2011', value: 4.5},
+      {dtg: '01-03-2011', value: 4.6},
+      {dtg: '01-04-2011', value: 3},
+      {dtg: '01-04-2011', value: 3},
+      {dtg: '01-04-2011', value: 3},
+      {dtg: '01-04-2011', value: 3.2},
+      {dtg: '01-04-2011', value: 3.2},
+      {dtg: '01-04-2011', value: 3.3},
+      {dtg: '01-04-2011', value: 3.3},
+      {dtg: '01-04-2011', value: 3.3},
+      {dtg: '01-04-2011', value: 3.4},
+      {dtg: '01-04-2011', value: 3.5},
+      {dtg: '01-04-2011', value: 3.7},
+      {dtg: '01-04-2011', value: 3.8},
+      {dtg: '01-04-2011', value: 4.2}
+    ],
+    data1: [
+      {
+        "item": "unicycles",
+        "average unit price": "$100",
+        "units sold": 34,
+        "revenue total": "$3,400"
+      },
+      {
+        "item": "weather vanes",
+        "average unit price": "$15",
+        "units sold": 67,
+        "revenue total": "$1,005"
+      },
+      {
+        "item": "catapults",
+        "average unit price": "$200",
+        "units sold": 5,
+        "revenue total": "$1,000"
+      },
+      {
+        "item": "harpoons",
+        "average unit price": "$50",
+        "units sold": 5,
+        "revenue total": "$250"
+      },
+      {
+        "item": "wheelbarrows",
+        "average unit price": "$40",
+        "units sold": 78,
+        "revenue total": "$3,120"
+      },
+      {
+        "item": "yo-yos",
+        "average unit price": "$13",
+        "units sold": 3000,
+        "revenue total": "$39,000"
+      },
+      {
+        "item": "blowguns",
+        "average unit price": "$45",
+        "units sold": 34,
+        "revenue total": "$1,530"
+      },
+      {
+        "item": "snow globes",
+        "average unit price": "$10",
+        "units sold": 5,
+        "revenue total": "$50"
+      },
+      {
+        "item": "didgeridoos",
+        "average unit price": "$30",
+        "units sold": 300,
+        "revenue total": "$9,000"
+      },
+      {
+        "item": "lava lamps",
+        "average unit price": "$20",
+        "units sold": 78,
+        "revenue total": "$1,560"
+      }
+    ],
     width: 450,
     height: 300
   }
@@ -19,7 +135,7 @@ class App extends Component {
             Tiffany France - D3 React Example
           </p>
 
-          <Histogram data={this.state.data} width={this.state.width} height={this.state.height}  />
+          <Histogram data={this.products.data} data1={this.products.data1} width={this.products.width} height={this.products.height}  />
         
         </header>
       </div>
--- src/histogram.js
+++ src/histogram.js
@@ -9,47 +9,81 @@ class Histogram extends Component {
 
   drawChart() {
     const data = this.props.data;
+    const data1 = this.props.data1;
 
-    const w = this.props.width;
-    const h = this.props.height;
+    const margin = {top: 10, right: 30, bottom: 30, left: 40},
+          width = this.props.width - margin.left - margin.right,
+          height = this.props.height - margin.top - margin.bottom;
 
-    const svg = d3.select("#foo")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-                  
+    let parseDate = d3.timeParse("%d-%m-%Y");
+
+    let x = d3.scaleTime()
+              .domain([new Date(2010, 6, 3), new Date(2012, 0, 1)])
+              .rangeRound([0, width]);
+    let y = d3.scaleLinear()
+              .range([height, 0]);
+
+    let histogram = d3.histogram()
+        .value((d) => d.date)
+        .domain(x.domain())
+        .thresholds(x.ticks(d3.timeMonth));
+
+    
+    let svg = d3.select("#viz").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+
+  // d3.csv('./earthquakes.csv', function(error, data) {
+  //   if (error) throw error;
+    
+    data.forEach((d) => d.date = parseDate(d.dtg));
+
+    data1.forEach((d) => {
+      d['average unit price'] = +d['average unit price'].replace(/\$|,/g, '');
+      d['revenue total'] = +d['revenue total'].replace(/\$|,/g, '');
+    });
+
+    let bins = histogram(data);
+
+    y.domain([0, d3.max(bins, (d) => d.length)]);
+
     svg.selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", (d, i) => i * 70)
-      .attr("y", (d, i) => h - 10 * d)
-      .attr("width", 65)
-      .attr("height", (d, i) => d * 10)
-      .attr("fill", "green")
+        .data(bins)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", 1)
+        .attr("transform", (d) => "translate(" + x(d.x0) + "," + y(d.length) + ")")
+        .attr("width", 15)
+        .attr("height", (d) => height - y(d.length))
+        .attr('fill', 'steelblue');
 
-    // const bar = svg.append("g")
-    //   .attr("fill", "steelblue")
-    //   .selectAll("rect")
-    //   .data(bins)
-    //   .join("rect")
-    //     .attr("x", d => x(d.x0) + 1)
-    //     .attr("width", d => Math.max(0, x(d.x1) - x(d.x0) - 1))
-    //     .attr("y", d => y(d.length))
-    //     .attr("height", d => y(0) - y(d.length));
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-    // svg.append("g")
-    //     .call(xAxis);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+        
+  // });
+
+  let client = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+  if (client < 500) {
+
+  } else {
     
-    // svg.append("g")
-    //     .call(yAxis);
+  }
 
-    
   
   }
 
   render(){
-    return <div id="foo"></div>
+    return <div id="viz"></div>
   }
 
 }
